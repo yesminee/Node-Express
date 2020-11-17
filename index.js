89bd19ac565bb8c9403d9182a532f52fad4218a0
yesminee <43929137+yesminee@users.noreply.github.com>
2020-11-17 12:09:57 +0100

    Add files via upload
    In this project :
        Implement a web server using the Express framework
        Develop a web server that supports a REST API
        Use Express router to implement support for the REST API
    to run this project type npm start

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishes');
+const promotionRouter = require('./routes/promotions');
+const leaderRouter = require('./routes/leader');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promotionRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
